e0b5de31b597ddc358ff447c32155a319cc15356
Andre Logan <andrelogan@Andres-MacBook-Air-2.local>
2026-03-20 21:46:12 -0400

    fix: logo on login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -77,7 +77,7 @@ return(
 
     {/* 🔥 YOUR IMAGE */}
     <img
-      src="/Logo.png" // <-- put your image in /public folder
+      src="blog/Logo.png" // <-- put your image in /public folder
       alt="Stoplee Golfer"
       style={{
         width: "100%",
