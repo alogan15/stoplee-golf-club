8014a30a8d1f1f88cb23325554a3f22a8695d34f
Andre Logan <andrelogan@Andres-MacBook-Air-2.local>
2026-04-25 18:02:01 -0400

    fixed auto zoom

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,10 @@
 import Navbar from "../components/Navbar"
 
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+    maximumScale: 1
+}
 
 export default function RootLayout({
   children,
