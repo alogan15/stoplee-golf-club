0d90691f76b986bc4c193b74da2bf0c070f0d167
Andre Logan <andrelogan@Andres-MacBook-Air-2.local>
2026-04-30 17:18:07 -0400

    removed sponsor

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -152,7 +152,7 @@ const [loading,setLoading] = useState(true)
                 <ActionCard title="📊 2026 Stats" onClick={() => router.push("/records")} />
                 <ActionCard title="👕 Merch" onClick={() => router.push("/merch")} />
                 <ActionCard title="📜 Rules" onClick={() => router.push("/rules")} />
-                <ActionCard title="🤝 Sponsorship" onClick={() => router.push("/sponsors")} />
+                {/* <ActionCard title="🤝 Sponsorship" onClick={() => router.push("/sponsors")} /> */}
                 </div>
                 
 
