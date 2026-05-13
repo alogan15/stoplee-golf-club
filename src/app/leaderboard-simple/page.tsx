49dd9384f2fb31070e9313690c61685249882c19
Andre Logan <andrelogan@Andres-MacBook-Air-2.local>
2026-05-13 13:00:28 -0400

    rock stats added

--- src/app/leaderboard-simple/page.tsx
+++ src/app/leaderboard-simple/page.tsx
@@ -27,145 +27,169 @@ const players: Player[] = [
     id: "2",
     name: "Malcolm",
     scores: [
-      { event: "Broad Run", points: 14, strokes: 100, isOfficial: true }    ]
+      { event: "Broad Run", points: 14, strokes: 100, isOfficial: true },
+      { event: "Rock Manor", points: 0, strokes: 0, isOfficial: false }     ]
   },
   {
     id: "3",
     name: "Aaron",
     scores: [
-      { event: "Broad Run", points:  20, strokes: 95, isOfficial: true }   ]
+      { event: "Broad Run", points:  20, strokes: 95, isOfficial: true },
+      { event: "Rock Manor", points: 0, strokes: 0, isOfficial: false }    ]
   },
     {
     id: "4",
     name: "Julian",
     scores: [
-      { event: "Broad Run", points:  5, strokes: 120, isOfficial: true }    ]
+      { event: "Broad Run", points:  5, strokes: 120, isOfficial: true },
+      { event: "Rock Manor", points: 0, strokes: 0, isOfficial: true }     ]
   },
   {
     id: "5",
     name: "Greg",
     scores: [
-      { event: "Broad Run", points: 1, strokes: 127, isOfficial: true }    ]
+      { event: "Broad Run", points: 1, strokes: 127, isOfficial: true },
+      { event: "Rock Manor", points: 0, strokes: 0, isOfficial: true }     ]
   },
   {
     id: "6",
     name: "LJ",
     scores: [
-      { event: "Broad Run", points:  11, strokes: 113, isOfficial: true }    ]
+      { event: "Broad Run", points:  11, strokes: 113, isOfficial: true },
+      { event: "Rock Manor", points: 0, strokes: 0, isOfficial: true }     ]
   },
     {
     id: "7",
     name: "Steph",
     scores: [
-      { event: "Broad Run", points: 9, strokes: 112, isOfficial: true }    ]
+      { event: "Broad Run", points: 9, strokes: 112, isOfficial: true },
+      { event: "Rock Manor", points: 0, strokes: 0, isOfficial: true }     ]
   },
   {
     id: "8",
     name: "Walt",
     scores: [
-      { event: "Broad Run", points: 4, strokes: 108, isOfficial: true }    ]
+      { event: "Broad Run", points: 4, strokes: 108, isOfficial: true },
+      { event: "Rock Manor", points: 0, strokes: 0, isOfficial: true }     ]
   },
   {
     id: "9",
     name: "James",
     scores: [
-      { event: "Broad Run", points:  6, strokes: 115, isOfficial: true }    ]
+      { event: "Broad Run", points:  6, strokes: 115, isOfficial: true },
+      { event: "Rock Manor", points: 0, strokes: 0, isOfficial: false }     ]
   },
     {
     id: "10",
     name: "Jamar",
     scores: [
-      { event: "Broad Run", points: 23, strokes: 95, isOfficial: true }    ]
+      { event: "Broad Run", points: 23, strokes: 95, isOfficial: true },
+      { event: "Rock Manor", points: 0, strokes: 0, isOfficial: true }     ]
   },
   {
     id: "11",
     name: "Larry",
     scores: [
-      { event: "Broad Run", points: 13, strokes: 104, isOfficial: true }    ]
+      { event: "Broad Run", points: 13, strokes: 104, isOfficial: true },
+      { event: "Rock Manor", points: 0, strokes: 0, isOfficial: true }     ]
   },
   {
     id: "12",
     name: "Greeco",
     scores: [
-      { event: "Broad Run", points:  10, strokes: 119, isOfficial: true }    ]
+      { event: "Broad Run", points:  10, strokes: 119, isOfficial: true },
+      { event: "Rock Manor", points: 0, strokes: 0, isOfficial: true }     ]
   },
     {
     id: "13",
     name: "Danny",
     scores: [
-      { event: "Broad Run", points: 4 , strokes: 122, isOfficial: true }    ]
+      { event: "Broad Run", points: 4 , strokes: 122, isOfficial: true },
+      { event: "Rock Manor", points: 0, strokes: 0, isOfficial: true }     ]
   },
   {
     id: "14",
     name: "Erik L",
     scores: [
-      { event: "Broad Run", points: 14, strokes: 106, isOfficial: true }    ]
+      { event: "Broad Run", points: 14, strokes: 106, isOfficial: true },
+      { event: "Rock Manor", points: 0, strokes: 0, isOfficial: true }     ]
   },
   {
     id: "15",
     name: "Isaiah",
     scores: [
-      { event: "Broad Run", points:  3, strokes: 119, isOfficial: true }    ]
+      { event: "Broad Run", points:  3, strokes: 119, isOfficial: true },
+      { event: "Rock Manor", points: 0, strokes: 0, isOfficial: true }     ]
   },
     {
     id: "16",
     name: "Tyrin",
     scores: [
-      { event: "Broad Run", points: 21, strokes: 93, isOfficial: true }    ]
+      { event: "Broad Run", points: 21, strokes: 93, isOfficial: true },
+      { event: "Rock Manor", points: 0, strokes: 0, isOfficial: true }     ]
   },
   {
     id: "17",
     name: "Short",
     scores: [
-      { event: "Broad Run", points: 9, strokes: 110, isOfficial: true }    ]
+      { event: "Broad Run", points: 9, strokes: 110, isOfficial: true },
+      { event: "Rock Manor", points: 0, strokes: 0, isOfficial: true }     ]
   },
   {
     id: "18",
     name: "Spurg",
     scores: [
-      { event: "Broad Run", points:  13, strokes: 0, isOfficial: true }    ]
+      { event: "Broad Run", points:  13, strokes: 0, isOfficial: true },
+      { event: "Rock Manor", points: 0, strokes: 0, isOfficial: true }     ]
   },
     {
     id: "19",
     name: "Cuffy",
     scores: [
-      { event: "Broad Run", points: 0, strokes: 0, isOfficial: true }    ]
+      { event: "Broad Run", points: 0, strokes: 0, isOfficial: true },
+      { event: "Rock Manor", points: 0, strokes: 0, isOfficial: true }     ]
   },
   {
     id: "20",
     name: "Jay",
     scores: [
-      { event: "Broad Run", points: 6, strokes: 122, isOfficial: false }    ]
+      { event: "Broad Run", points: 6, strokes: 122, isOfficial: false },
+      { event: "Rock Manor", points: 0, strokes: 0, isOfficial: false }     ]
   },
   {
     id: "21",
     name: "Keivon",
     scores: [
-      { event: "Broad Run", points:  16, strokes: 106, isOfficial: false }    ]
+      { event: "Broad Run", points:  16, strokes: 106, isOfficial: false },
+      { event: "Rock Manor", points: 0, strokes: 0, isOfficial: false }     ]
   },
     {
     id: "22",
     name: "Anthony",
     scores: [
-      { event: "Broad Run", points:  0, strokes: 0, isOfficial: false  }    ]
+      { event: "Broad Run", points:  0, strokes: 0, isOfficial: false  },
+      { event: "Rock Manor", points: 0, strokes: 0, isOfficial: true }     ]
   },
   {
     id: "23",
     name: "Eric B",
     scores: [
-      { event: "Broad Run", points: 0, strokes: 0, isOfficial: false  }    ]
+      { event: "Broad Run", points: 0, strokes: 0, isOfficial: false  },
+      { event: "Rock Manor", points: 0, strokes: 0, isOfficial: true }     ]
   },
     {
     id: "24",
     name: "Rickey",
     scores: [
-      { event: "Broad Run", points: 5, strokes: 0, isOfficial: true  }    ]
+      { event: "Broad Run", points: 5, strokes: 0, isOfficial: true  },
+      { event: "Rock Manor", points: 0, strokes: 0, isOfficial: true }     ]
   },
     {
     id: "24",
     name: "Justin",
     scores: [
-      { event: "Broad Run", points: 7, strokes: 118, isOfficial: false  }    ]
+      { event: "Broad Run", points: 7, strokes: 118, isOfficial: false  },
+      { event: "Rock Manor", points: 0, strokes: 0, isOfficial: false }     ]
   },
 ]
 
